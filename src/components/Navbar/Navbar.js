5aaff7f7f5fbdd140313a2f12bacec552d3d2685
pournimaanjole <pournimaanjole@gmail.com>
2023-09-13 17:00:54 +0530

    adding second navbar in this webpage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,29 @@
 import './Navbar.css';
 import imglogo from './Navbarimges/starbucks.png';
- export default function Navbar(){
-    return(<>
-    <header className='header '>
-        <nav className='navbar'>
-            <img src={imglogo} alt='logo img' className='logo-img'></img>
-            <ul className='nav-list'>
-                <li><a href='/' className='nav-link'>Home</a></li>
-                <li><a href='/' className='nav-link'>Gift</a></li>
-                <li><a href='/' className='nav-link'>Order</a></li>
-                <li><a href='/' className='nav-link'>pay</a></li>
-                <li><a href='/' className='nav-link'>Store</a></li>
-            </ul>
-        </nav>
-        <div className=''>
-           <button><i class="fa-solid fa-magnifying-glass"></i> <input></input></button>
-           
+export default function Navbar() {
+    return (<>
+        <header className='header '>
+            <nav className='navbar'>
+                <img src={imglogo} alt='logo img' className='logo-img'></img>
+                <ul className='nav-list'>
+                    <li><a href='/' className='nav-link'>Home</a></li>
+                    <li><a href='/' className='nav-link'>Gift</a></li>
+                    <li><a href='/' className='nav-link'>Order</a></li>
+                    <li><a href='/' className='nav-link'>pay</a></li>
+                    <li><a href='/' className='nav-link'>Store</a></li>
+                </ul>
+            </nav>
+            <div className='search-bar'>
+                <div className='search-button'><i class="fa-solid fa-magnifying-glass"></i> <input placeholder='Looking for something specific?' className='search-box'></input></div>
+                <div className='contact-icon'><i class="fa-regular fa-user"></i></div>
 
-
+            </div>
+        </header>
+        <div className='second-header'>
+            <p>Avail benefits!Sign up for My Starbucks Rewards.</p>
+            <button>Know More</button>
 
         </div>
-    </header>
 
     </>)
 }
