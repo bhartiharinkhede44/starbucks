9e611c72b59b8bc497d055d4a38e56e1bb682278
saurabh jaykar <saurabh@kali.kali.org>
2023-09-14 17:27:52 +0530

    feat : headercard comp added

--- src/views/Gift/Gift.js
+++ src/views/Gift/Gift.js
@@ -1,59 +1,15 @@
 import React from "react";
 import "./Gift.css";
 import "./../../index.css";
-import HeaderImg from "./images/banner_icon_1_feafe78e1b.png";
+import HeadImg from "./images/banner_icon_1_feafe78e1b.png";
 import Button from "../../components/Button/Button";
+import HeaderCard from "../../components/HeaderCard/HeaderCard";
 
 function Gift() {
   return (
     <>
-    <div>
-</div>
-      <header>
-        <div className="gift-card-header-container bg-green">
-          <div className="gift-card-container border-radius">
-            <div className="container d-flex align-items-center">
-              <div className="card1">
-                <div className="container d-flex  ">
-                  <img src={HeaderImg} alt="" className="header-img" />
-                  <div className="container  p-10">
-                    <div className="d-flex flex-column">
-                      <div className="p-10 ">
-                        <p>New Season, New Sips!</p>
-                      </div>
-                      <div className="mt-10 ">
-                        <h2>Autum Merchandise</h2>
-                      </div>
-                      <div className="mt-15">
-                        <p className="fw-lighter">
-                          Embrace the Fall vibes as you sip in style with autum
-                          merchandise featuring super moon and bunny.
-                        </p>
-                      </div>
-                      <div className="mt-22 ">
-                        <p>starting From</p>
-                        <h3> ₹ 650</h3>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="card2 d-flex ">
-                <div className="btn-card1 " >
-
-                </div>
-                <div className="btn-card2">
-                  <div className="custom-btn-margin p-8">
-
-<Button btnName={"Order Now"}/>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <div></div>
+      <HeaderCard HeaderImg={HeadImg} HeaderOne={"New Season, New Sips!"} HeaderTwo={"Autum Merchandise"} Description={"Embrace the Fall vibes as you sip in style with autummerchandise featuring super moon and bunny."} StartPriceTitle={"Starting Price"} Price={" ₹ 650"} />
     </>
   );
 }
