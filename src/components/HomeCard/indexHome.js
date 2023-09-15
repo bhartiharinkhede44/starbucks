dc4bbb1046fb62d25fe9e3ee3c1b77d8c58fcf28
Poonam Digole <poonamdigole123@gmail.com>
2023-09-15 18:36:14 +0530

    cards created

---
+++ src/components/HomeCard/indexHome.js
@@ -0,0 +1,17 @@
+import './indexHome.css';
+import Button from '../Button/Button';
+function HomeCard({title, description , about, img , prize}) {
+    return (
+      <div className="home-card">
+            <img src={img} className='bevarage'/>
+          <h2 className='heading'>{title}</h2>
+          <p className='about'>{about}</p>
+          <p className='descript'>{description}</p>
+          <span className='prize'> {prize} </span>
+          <Button btnName="Add Item" CustomBtn="btn" />
+        
+      </div>
+    )
+  }
+  
+  export default HomeCard;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -12,74 +12,104 @@ import pumpkinimg from './imgesforhomepage/pumpkin.png';
 import imgeof from './imgesforhomepage/background.jpg';
 import Button from '../../components/Button/Button';
 import Recomands from '../../components/Recomands/Recomands';
+// import HomeCard from './../../components/HomeCard/indexHome';
+import './../../components/HomeCard/HomeCard.css';
+import HomeCard from './../../components/HomeCard/indexHome';
+import img1 from './../../components/HomeCard/bevarage01.png';
+import img2 from './../../components/HomeCard/bevarage02.png';
+import img3 from './../../components/HomeCard/bevarage03.png';
 
 
 
 
 
-export default function Home(){
-    return(<>
-    <Navbar/>
-    <div className='heading-for-handcraft'><h2>Handcrafted Curations</h2>
-    <div className='handcrafedt-inline'>
-        <div   >
-        <img src={imgs1}  className='handcrafted-img'></img>
-        <p className='para'>Bestseller</p>
+
+export default function Home() {
+    return (<>
+        <Navbar />
+        <div className='heading-for-handcraft'><h2>Handcrafted Curations</h2>
+            <div className='handcrafedt-inline'>
+                <div   >
+                    <img src={imgs1} className='handcrafted-img'></img>
+                    <p className='para'>Bestseller</p>
+                </div>
+
+                <div >
+                    <img src={imgs2} className='handcrafted-img'></img>
+                    <p>CoffeeAtHome</p>
+                </div>
+
+                <div >
+                    <img src={imgs3} className='handcrafted-img'></img>
+                    <p className='para'>Drinks</p>
+                </div>
+
+                <div >
+                    <img src={imgs4} className='handcrafted-img'></img>
+                    <p className='para'>Food</p>
+                </div>
+
+                <div >
+                    <img src={imgs5} className='handcrafted-img'></img>
+                    <p className='para'>Merchandise</p>
+                </div>
+
+                <div >
+                    <img src={imgs6} className='handcrafted-img'></img>
+                    <p className='para'>ReadyToEat</p>
+                </div>
+            </div>
         </div>
 
-        <div >
-        <img src={imgs2} className='handcrafted-img'></img>
-        <p>CoffeeAtHome</p>
+        <HeaderCard HeaderImg={pumpkinimg}
+            HeaderOne={"Guess Whose Back!"}
+            HeaderTwo={"Pumpkin spice menu"}
+            Description={
+                "prepaire to rekindle autumn memories with the much-awaited return of the beloved pumpkin spice menu."
+            }
+            StartPriceTitle={"Starting Price"}
+            Price={" ₹ 400.00"}
+        />
+        {/*  */}
+
+
+
+
+        <div className='home-container'>
+           <HomeCard img={img1} title="Pumpkin spice latte"  about="SHORT(237ML.216kcal) " description="Our signarture espresso 
+            and steamed milk with the celebrated flavour combination of pumpkin , cinnamon , 
+             nutmeg and... " prize="414.75/-" />
+
+
+           <HomeCard img={img3} title="Pumpkin spice latte" about="TALL(Tall Pumpkin Spice Frappuccino).370 kcal "
+            description="Our signarture espresso 
+            and steamed milk with the celebrated flavour combination of pumpkin , cinnamon , 
+             nutmeg and... " prize="414.75/-" />
+
+           <HomeCard img={img2} title="Pumpkin spice latte" about="TALL(Tall Pumpkin Spice Brewed Coffee BREWED COFFEE).261 kcal " description="Our signarture espresso 
+            and steamed milk with the celebrated flavour combination of pumpkin , cinnamon , 
+             nutmeg and... " prize="414.75/-" />
+
+       </div>
+
+        {/*  */}
+        {/* linera gradiant footer vala   */}
+        <div className='world-of-coffee'>
+            <h2 >Learn more about the world of coffee!</h2>
+            <div className='backgroudn-img'>
+                <h1>The Art And Science Of Sampling Coffee</h1>
+                <p>Explore the world of Sampling coffee at Starbucks and uncover the Art and science  
+                      behind creating perfect coffee.</p>
+                <Button btnName="learn more" CustomBtn="custombtn" />
+            </div>
+
         </div>
+        {/* linera gradiant footer vala   */}
 
-        <div >
-        <img src={imgs3} className='handcrafted-img'></img>
-        <p className='para'>Drinks</p>
-        </div>
-
-        <div >
-        <img src={imgs4} className='handcrafted-img'></img>
-        <p className='para'>Food</p>
-        </div>
-
-        <div >
-        <img src={imgs5} className='handcrafted-img'></img>
-        <p className='para'>Merchandise</p>
-        </div>
-
-        <div >
-        <img src={imgs6} className='handcrafted-img'></img>
-        <p className='para'>ReadyToEat</p>
-        </div>
-    </div>
-    </div>
-
-    <HeaderCard HeaderImg={pumpkinimg}
-     HeaderOne={"Guess Whose Back!"} 
-     HeaderTwo={"Pumpkin spice menu"}
-     Description={
-       "prepaire to rekindle autumn memories with the much-awaited return of the beloved pumpkin spice menu."
-     }
-     StartPriceTitle={"Starting Price"}
-     Price={" ₹ 400.00"}
-    />
-{/*  */}
-
-
-    {/*  */}
-    {/* linera gradiant footer vala   */}
-    <div className='world-of-coffee'>
-      <h2 >Learn more about the world of coffee!</h2>
-      <div className='backgroudn-img'>
-        <h1>The Art And Science Of Sampling Coffee</h1>
-        <p>Explore the world of Sampling coffee at Starbucks and uncover the Art and science  behind creating perfect coffee.</p>
-        <Button btnName="learn more" CustomBtn="custombtn"/>
-      </div>
-    
-    </div>
-    {/* linera gradiant footer vala   */}
-
-    <Footer/>
+        <Footer />
 
     </>)
 }  
+
+
+
